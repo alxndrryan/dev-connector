71dbe3e891b31a931af5d6e17e325ed57583aa12
Rand al'Thor <alexanderhogle@gmail.com>
2020-10-11 14:12:23 -0500

    fixes typo

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -29,7 +29,7 @@ export const Login = ({ login, isAuthenticated }) => {
     <Fragment>
       <h1 className='large text-primary'>Sign In</h1>
       <p className='lead'>
-        <i className='fas fa-user'></i> Sing Into Your Account
+        <i className='fas fa-user'></i> Sign Into Your Account
       </p>
       <form className='form' onSubmit={(e) => onSubmit(e)}>
         <div className='form-group'>
